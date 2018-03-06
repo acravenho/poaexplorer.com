a0ca1936b447c6350e374ffea6ff53d6e59e5731
Andrew Cravenho <andrew@cravenho.me>
2018-03-05 19:02:15 -0500

    Updated API documentation.

--- application/views/tools/api.php
+++ application/views/tools/api.php
@@ -1,7 +1,53 @@
 <div class="row">
 	<div class="col-md-12">
 		<h2>POA Network API</h2>
-		<p class="lead">Coming Soon.</p>
+		<hr />
+		<div class="row">
+			<div class="col-md-2">
+				
+			</div>
+			<div class="col-md-8">
+				<p class="lead">Network Stats</p>
+				<hr />
+				<figure class="highlight">
+					<pre>https://poaexplorer.com/api/network</pre>
+				</figure>
+				
+				<br /><br />
+				<p>Results:</p>
+				<figure class="highlight">
+					<pre>
+{ 
+	"average_block_time":"5",
+	"last_block_timestamp":"1520293559",
+	"last_block_number":"1377817",
+	"circulating_supply":203346457,
+	"total_supply":253838617
+}
+					</pre>
+				</figure>
+				
+				<br /><br /><br /><br />
+				
+				<p class="lead">Account Balance</p>
+				<hr />
+				<figure class="highlight">
+					<pre>https://poaexplorer.com/api/balance/0x3091aee5cb7a290da8e05cc4b70dac7715de39a0</pre>
+				</figure>
+				<br /><br />
+				<p>Results:</p>
+				<figure class="highlight">
+					<pre>
+{
+	"status":"ok",
+	"address":"0x3091aee5cb7a290da8e05cc4b70dac7715de39a0",
+	"balance":34,
+	"transactions_count":741
+}					
+					</pre>
+				</figure>
+			</div>
+		</div>
 		
 	</div>
 	
